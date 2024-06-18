f58544a073b956ff053700c7affdf215fbadc6de
Kevin Hernández <kevingeogarcia@gmail.com>
2024-06-17 22:37:01 -0600

    Pipes númericos y Pipes no comunes

--- src/app/products/pages/number-page/number-page.component.ts
+++ src/app/products/pages/number-page/number-page.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class NumberPageComponent {
 
+  public totalSells: number = 2567789.5567;
+  public percent: number = 0.4856;
 }
--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -7,4 +7,48 @@ import { Component } from '@angular/core';
 })
 export class UncommonPageComponent {
 
+
+  //i18n Select
+  public name: string = 'Kevin';
+  public gender: 'male' | 'female' = 'male';
+  public invitationMap = {
+    male: 'invitarlo',
+    female: 'invitarla',
+  }
+  changeClient(): void {
+    console.log(this.gender);
+    if (this.gender === 'male') {
+      this.name = 'Melissa'
+      this.gender = 'female'
+    } else {
+      this.name = 'Kevin'
+      this.gender = 'male'
+    }
+  }
+
+
+  //i18n Plural
+  public clients: string[] = ['Kevin', 'Geovani', 'Carlos','Jose'];
+
+  public clientsMap = {
+    '=0': 'no tenemos ningun cliente esperando.',
+    '=1': 'tenemos un cliente esperando.',
+    '=2': 'tenemos dos clientes esperando.',
+    '=3': 'tenemos 3 clientes esperando la reserva.',
+    'other': 'tenemos # clientes esperando.',
+  }
+
+  deleteClient(): void {
+    console.log(this.clients.length);
+    this.clients.shift();
+  }
+
+
+  // keyvalue pipe
+  public person = {
+    name: 'Kevin',
+    age: 25,
+    address: 'San Salvador, El Salvador'
+  }
+
 }
